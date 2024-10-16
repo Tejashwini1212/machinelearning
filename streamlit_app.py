8f2f95c8f2da75a22a2770a411fecee26e125af7
Tejashwini Hosamani <121215868+Tejashwini1212@users.noreply.github.com>
2024-10-16 19:19:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.title('🦾Machine Learning App')
 
 st.info('This application constructs a machine learning model!')
 
+with st.expander('Data'):
+  st.write('** 📊Raw data**')
 df = pd.read_csv('https://raw.githubusercontent.com/Tejashwini1212/dataset/refs/heads/main/iris.csv')
 
 df
