4e190695d778cd147c292c4518e31cd8629b6975
Tejashwini Hosamani <121215868+Tejashwini1212@users.noreply.github.com>
2024-10-16 21:39:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,26 @@
-import streamlit as st
+mport streamlit as st
 import pandas as pd
-import altair as alt
 
 st.title('🦾Machine Learning App')
 
 st.info('This application constructs a machine learning model!')
 
 with st.expander('Data'):
-    st.write('**Raw data**')
-    df = pd.read_csv('https://raw.githubusercontent.com/Tejashwini1212/dataset/refs/heads/main/iris.csv')
-    st.write(df)  # Display the dataframe
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Tejashwini1212/dataset/refs/heads/main/iris.csv')
+  df
 
-    st.write('**X**')
-    X = df.drop('Sepal.Length', axis=1)
-    st.write(X)
+  st.write('**X**')
+  X = df.drop('Sepal.Length', axis=1)
+  X
 
-    st.write('**y**')
-    y = df['Sepal.Length']
-    st.write(y)
-
+  
+  st.write('**y**')
+  y = df['Sepal.Length']
+  y
+  
 with st.expander('Data Visualization'):
-    # Use Altair for creating the scatter plot with color encoding
-    scatter_plot = alt.Chart(df).mark_circle(size=60).encode(
-        x='Sepal.Length',
-        y='Petal.Width',
-        color='Sepal.Width',
-        tooltip=['Sepal.Length', 'Sepal.Width', 'Petal.Length', 'Petal.Width', 'Species']  # Tooltip to show details
-    ).interactive()
-
-    st.altair_chart(scatter_plot, use_container_width=True)
-
+    st.scatter_chart(data=df, x='Sepal.Length', y='Petal.Width', color='Sepal.Width')
 
 
 
