0a43bc777211b8199ef0a5b3dcc9bc83c4fbff3f
Tejashwini Hosamani <121215868+Tejashwini1212@users.noreply.github.com>
2024-10-17 02:33:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,8 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
-prediction_proba
+df_prediction_proba = pd.DataFrame(prediction_proba)
+
 
 
 
