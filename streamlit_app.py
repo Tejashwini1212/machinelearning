b5407942fb4b6adbbad6e26e09546f9939bb7466
Tejashwini Hosamani <121215868+Tejashwini1212@users.noreply.github.com>
2024-10-17 01:25:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,8 @@ data = {'island': island,
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
 
+with st.expander{'Input features'):
+st.write('**Input Penguins**')
 input_penguins
 
 
