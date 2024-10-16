897cb2d8444085b38e6b476910efc6f0e8cfa6e1
Tejashwini Hosamani <121215868+Tejashwini1212@users.noreply.github.com>
2024-10-17 00:38:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,23 @@ st.title('🦾Machine Learning App')
 # Information
 st.info('This application constructs a machine learning model!')
 
-# Loading and displaying the data
+
+
+
 with st.expander('Data'):
     st.write('**Raw data**')
     
     # Load the data from the URL
     df = pd.read_csv('https://raw.githubusercontent.com/Tejashwini1212/dataset/main/penguins_size.csv')
     df
+
+    st.write('**X**)
+    X = df.drop('species', axis=1)
+    X
+
+    st.write('**y**)
+    y = df.species
+    y
     
+
+ 
